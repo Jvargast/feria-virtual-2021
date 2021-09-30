1a82abfa3179ad04b7181dd443bcf1b03551e6f3
Jvargast <jose.vargas.t@csi.cl>
2021-09-30 20:47:56 -0300

    not found component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import {BrowserRouter, Switch, Route}  from 'react-router-dom';
 import Auth from './components/auth/Auth';
+import NotFound from './components/NotFound/NotFound';
 import ParkView from './components/protected/ParkView';
 import ProtectedRoute from './components/protected/ProtectedRoute';
 
@@ -13,6 +14,7 @@ function App(): JSX.Element {
         <Auth/>
       </Route>
       <ProtectedRoute exact path="/homepage" component={ParkView}/>
+      <Route path="*" component={NotFound}/>
       
     </Fragment>
    
